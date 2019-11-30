d66e8aba63fcbe74a97ba9bb8ed354ed004f2074
Eduardo Fantini <edufantinicod@gmail.com>
2019-11-30 15:32:39 -0300

    AVL

---
+++ avl.h
@@ -0,0 +1,24 @@
+#include "descritor.h"
+#define MAX_PALAVRA 50
+
+typedef struct AVLNode {
+
+    char texto[MAX_PALAVRA];
+    int freq;
+    int fator;
+    struct AVLNode *esq; 
+    struct AVLNode *dir;
+
+} AVLNode;
+
+AVLNode *insereAVL(AVLNode *pt, char txt[MAX_PALAVRA], descritor *dscr, int *cond);
+AVLNode *condPositive(AVLNode *pt, descritor *dscr, int *cond);
+AVLNode *condNegative(AVLNode *pt, descritor *dscr, int *cond);
+AVLNode *rdAVL(AVLNode *pt, descritor *dscr);
+AVLNode *reAVL(AVLNode *pt, descritor *dscr);
+AVLNode *rddAVL(AVLNode *pt, descritor *dscr);
+AVLNode *rdeAVL(AVLNode *pt, descritor *dscr);
+
+int alturaAVL(AVLNode *pt);
+int calcFatorAVL(AVLNode *pt);
+int isAVL(AVLNode *pt);
